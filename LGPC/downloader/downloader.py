976d42ee6699f507d701300913b91a08d42b67ea
aaaron7 <aaaron7@outlook.com>
2021-01-31 21:50:55 +0800

    drafting project strcuture.
    drafting project strcuture.

---
+++ LGPC/downloader/downloader.py
@@ -0,0 +1,33 @@
+# author:aaron
+# filename:downloader.py
+# date: "2021-01-31"
+
+import sys
+# sys.path.append("C:\\Users\\appri\\Documents\\GitHub\\LGPC")
+import json
+import os
+from os import listdir
+from os.path import isfile, join
+import requests
+import time
+import random
+import json
+import urllib
+from LGPC.utils import status
+# import LGPC.utils.status
+class Downloader:
+    def __init__(self):
+        pass
+
+    # def 
+    
+class Request:
+    def __init__(self, url):
+        self.url = url
+        self.status = status.Status()
+        
+
+if __name__ == "__main__":
+    print("Hello")
+    pass
+
---
+++ LGPC/utils/status.py
@@ -0,0 +1,6 @@
+
+class Status:
+    def __init__(self, code = 0, msg = ""):
+        self.code = code
+        self.msg = msg
+    
